618e6e06d609ac1bd5105811338febf4c1927884
Chris Lamb <chris@primate.co.uk>
2014-10-14 13:20:57 +0100

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,89 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var concat = require('gulp-concat');
-
+// var concat = require('gulp-concat'); // Production task needs to be created
 var fs = require("fs");
 var coffee = require('gulp-coffee');
 var sass = require('gulp-ruby-sass');
-var compass = require('gulp-compass');
 var plumber = require('gulp-plumber');
 var s3 = require("gulp-s3");
+var prefix = require('gulp-autoprefixer');
 
+//**********************************************************************
+// PROJECT DIRECTORIES
+//**********************************************************************
 var paths = {
-  scripts: ['../locomotive_sites/app/assets/javascripts/*.coffee'],
-  compass: '../locomotive_sites/app/assets/stylesheets/**/*.scss'
+  scripts_in: ['../locomotive_sites/app/assets/javascripts/*.coffee'],
+  scripts_out: '../locomotive_sites/public/javascripts',
+  sass_in: ['../locomotive_sites/app/assets/stylesheets/*.scss'],
+  sass_out: '../locomotive_sites/public/stylesheets',
+  public_assets: ['../locomotive_sites/public/**'],
+  font_assets: ['../locomotive_sites/public/fonts/**']
 };
 
-//*
-// COFFEE -> JavaScript
-//*
+//**********************************************************************
+// COFFEE -> JAVASCRIPT
+//**********************************************************************
 gulp.task('scripts', function(){
 
-  return gulp.src(paths.scripts)
+  return gulp.src(paths.scripts_in)
     .pipe(plumber())
     .pipe(coffee({
       bare: true
     }))
-    .pipe(gulp.dest('../locomotive_sites/public/javascripts'));
+    .pipe(gulp.dest(paths.scripts_out));
 });
 
-//*
+//**********************************************************************
 // SASS -> CSS
-//*
+//**********************************************************************
 gulp.task('sass', function () {
-  gulp.src('../locomotive_sites/app/assets/stylesheets/*.scss')
-      .pipe(sass({sourcemap: true}))
-      .pipe(gulp.dest('../locomotive_sites/public/stylesheets'))
+  gulp.src(paths.sass_in)
+      .pipe(sass({sourcemap: false}))
+      .pipe(prefix())
+      .pipe(gulp.dest(paths.sass_out))
 });
 
-//*
-// WATCHER -- RUNS SCRIPT AND SASS TASKS
-//
-//*
+//**********************************************************************
+// WATCHERS -- RUNS SCRIPT AND SASS TASKS
+//**********************************************************************
 gulp.task('watch', function () {
-  gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.compass, ['compass']);
+  gulp.watch(paths.scripts_in, ['scripts']);
+  gulp.watch(paths.sass_in, ['sass']);
 });
 
-//*
+//**********************************************************************
 // FONT ICON UPLOAD
 // Upload font icons to S3.  You need to specify IAM keys.
-//*
-gulp.task('fonticontos3', function() {
+//**********************************************************************
+gulp.task('fonts-to-s3', function() {
 
   // UPDATE IAM KEYS - create 'keys' folder
-  aws = JSON.parse(fs.readFileSync('./keys/updateme.aws.json'));
+  aws = JSON.parse(fs.readFileSync('./keys/xxx.aws.json'));
 
-  // The delay needs to be kept, otherwise the transfer fails.
-  options = { delay: 1000 }
+  options = {
+    delay: 1000, // The delay needs to be kept, otherwise the transfer fails
+    uploadPath: '/fonts/'
+  }
 
-  gulp.src('../locomotive_sites/public/fonts/**', {read: false})
+  gulp.src(paths.font_assets, {read: false})
       .pipe(s3(aws, options));
 
 });
 
+//**********************************************************************
+// S3 - MOVE ALL PUBLIC ASSETS TO S3
+// You need to specify IAM keys.
+//**********************************************************************
+gulp.task('assets-to-s3', function() {
+
+  aws = JSON.parse(fs.readFileSync('./keys/xxx.aws.json'));
+
+  options = { delay: 1000 } // The delay needs to be kept, otherwise the transfer fails.
+
+  gulp.src(paths.public_assets, {read: false})
+      .pipe(s3(aws, options));
+
+});
+
+
 gulp.task('default', ['watch']);
-gulp.task('font_upload', ['fonticontos3']);
